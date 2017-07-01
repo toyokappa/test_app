d64723c8062d3725da37ee10534e162910fce022
toyokappa <kppg42@gmail.com>
2017-07-01 23:33:25 +0900

    Layoutの変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  #ページごとのタイトルを設定
+  def full_title(page_title = "")
+    base_title = "Knowledge Stocker"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
