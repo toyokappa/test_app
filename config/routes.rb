423f7d792a2178738f67203fdd9d4ea921942c97
toyokappa <kppg42@gmail.com>
2017-07-07 06:47:58 +0900

    Add signup_path and users resourse to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  get '/signup' , to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
 end
